69264612718972209eae1acf2aa3d30fccd4d296
todsung <wlunareve@gmail.com>
2024-08-12 01:49:42 +0800

    feat: modify layout

---
+++ src/components/Section.tsx
@@ -0,0 +1,51 @@
+import { ReactNode } from "react";
+import { StyleSheet, View } from "@react-pdf/renderer";
+import Typography from "./Typography";
+import { PALETTE } from "../modules/Resume/Resume.const";
+
+
+const styles = StyleSheet.create({
+  section: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingBottom: 12,
+  },
+
+  titleWrapper: {
+    width: 120,
+  },
+
+  title: {
+    color: PALETTE.font
+  },
+
+  childrenWrapper: {
+    flex: 1,
+  },
+  
+  sectionBorder: {
+    borderBottom: `2px solid ${PALETTE.border}`
+  },
+});
+
+
+interface SectionProps {
+  title: string;
+
+  withBorder?: true;
+
+  children: ReactNode;
+}
+
+const Section = ({ title, withBorder, children }: SectionProps) => (
+  <View style={{...styles.section, ...withBorder && styles.sectionBorder }}>
+    <View style={styles.titleWrapper}>
+      <Typography variant='h5' style={styles.title}>{title}</Typography>
+    </View>
+    <View style={styles.childrenWrapper}>
+      {children}
+    </View>
+  </View>
+)
+
+export default Section;
--- src/modules/Resume/Resume.tsx
+++ src/modules/Resume/Resume.tsx
@@ -5,12 +5,12 @@ import DeepQ from './Experience/DeepQ';
 import IISI from './Experience/IISI';
 import { PALETTE } from './Resume.const';
 import Typography from '../../components/Typography';
-import ListItem from '../../components/ListItem';
 import Skills from './Experience/Skills';
+import Section from '../../components/Section';
 
 const styles = StyleSheet.create({
   main: {
-    margin: 12,
+    margin: 10,
     flexDirection: 'column',
     flex: 1,
     gap: 8,
@@ -23,6 +23,9 @@ const styles = StyleSheet.create({
 
   section: {
     paddingBottom: 12,
+  },
+  
+  sectionBorder: {
     borderBottom: `2px solid ${PALETTE.border}`
   },
 
@@ -31,7 +34,7 @@ const styles = StyleSheet.create({
   },
 
   skills: {
-    gap: 8
+    gap: 4
   }
 });
 
@@ -42,28 +45,26 @@ const Resume = () => (
         <View style={styles.profileSection}>
           <Profile />
         </View>
-        <View style={styles.section}>
-          <Typography variant='h4'>About Me</Typography>
+        <Section title="About Me" withBorder>
           <Typography variant='body-md'>
             我是 Tod，是嚴謹的前端工程師，專注於產出的每一個細節，重視成果的實際體現。
-            擁有五年網頁開發經驗，專長於 React 生態系的系統開發、重構，具備打造卓越產品的自信；在團隊中常做為承上啟下的角色，能夠有效的在開發團隊間溝通；
+            擁有五年網頁開發經驗，專長於 React 生態系的系統開發、重構，具備打造卓越產品的自信；
+            在團隊中常做為承上啟下的角色，能夠有效的在開發團隊間溝通；
             工作之餘經常閱讀技術文章，並於團隊合作中提出觀點，致力於提升自己或是團隊的工作效率。
           </Typography>
-        </View>
-        <View style={styles.section}>
-          <Typography variant='h4'>Work Experience</Typography>
+        </Section>
+        <Section title="Experience" withBorder>
           <View style={styles.companies}>
             <Noodoe />
             <DeepQ />
             <IISI />
           </View>
-        </View>
-        <View style={styles.section}>
-          <Typography variant='h4'>Key Skills</Typography>
+        </Section>
+        <Section title="Key Skills">
           <View style={styles.skills}>
             <Skills />
           </View>
-        </View>
+        </Section>
       </View>
     </Page>
   </Document>
